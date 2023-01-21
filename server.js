37f245a6e0203847ed19a66986868802e40442bd
Marko1310 <marko.cabo13@gmail.com>
2023-01-21 18:35:29 +0100

    Handling ./signin and ./register

--- server.js
+++ server.js
@@ -1,20 +1,66 @@
 const express = require("express");
 const port = 8000;
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "1234",
+      name: "SALLY",
+      email: "SALLY@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
 
 app.get("/", (req, res) => {
-  res.send("this is working");
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error login in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
 });
 
 app.listen(port, () => {
-  console.log(`app is runnong on ${port}`);
+  console.log(`app is running on ${port}`);
 });
-
 
 /*
 / --> res = this is workin
 /signin --> POST = success/fail
 /register --> POST = user
 /profile/:userId --> GET = user
-/image --> PUT --> user
+/image --> PUT --> user*/
