9f8235f682a3c220c10e99788a635d1556ff3476
Marko1310 <marko.cabo13@gmail.com>
2023-01-23 20:33:44 +0100

    registering a user into the database

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 
-const postgres = knex({
+const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1/",
+    host: "127.0.0.1",
     port: 5432,
     user: "marko",
     password: "",
@@ -16,7 +16,11 @@ const postgres = knex({
   },
 });
 
-console.log(postgres.select("*").from("users"));
+db.select("*")
+  .from("users")
+  .then((data) => {
+    console.log(data);
+  });
 
 const app = express();
 
@@ -83,16 +87,16 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  bcrypt.hash(password, null, null, function (err, hash) {
-    console.log(hash);
-  });
-  database.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date(),
-  });
+  // bcrypt.hash(password, null, null, function (err, hash) {
+  //   console.log(hash);
+  // });
+  db("users")
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date(),
+    })
+    .then(console.log());
   res.json(database.users[database.users.length - 1]);
 });
 
