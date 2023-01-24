5a493c7eccd11e4e5caf6f6141d00d43d593e1be
Marko1310 <marko.cabo13@gmail.com>
2023-01-24 13:08:16 +0100

    Implement bcrypt into signin

--- server.js
+++ server.js
@@ -60,29 +60,27 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  bcrypt.compare(
-    "apples",
-    "$2a$10$mwVYtAA0tK/K7x3olk2B/.RxSmm/5eApd1QA.PuxTm9CI1rdIvxZO",
-    function (err, res) {
-      console.log("1.guess", res);
-    }
-  );
-  bcrypt.compare(
-    "veggies",
-    "$2a$10$mwVYtAA0tK/K7x3olk2B/.RxSmm/5eApd1QA.PuxTm9CI1rdIvxZO",
-    function (err, res) {
-      console.log("2.guess", res);
-    }
-  );
-
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json("error login in");
-  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      console.log(isValid);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            console.log(user);
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credentials"));
 });
 
 app.post("/register", (req, res) => {
